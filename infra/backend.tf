db49999a77aa78a4690a3ec26506f27d5a252d53
David Avila <33632789+davidaavilar@users.noreply.github.com>
2024-12-09 14:24:10 -0500

    updated backend variables

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "${var.s3_backend}"
-    key    = "${var.s3_backend_key}"
-    region = "${var.region}"
+    bucket = ""
+    key    = ""
+    region = ""
   }
 }
